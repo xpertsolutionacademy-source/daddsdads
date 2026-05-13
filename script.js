cd4f1408f7e8465255c66d60c8ccb7a8a0ff21de
XPERT SOLUTION ACADEMY <xpertsolutionacademy@gmail.com>
2026-05-13 22:42:56 +0500

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+// var myName= null;
+// console.log(myName);
+// console.log(typeof null);
+
+
+
+
+// var badMemories= null;
+// console.log(badMemories);
+// console.log(typeof null);
+
+
+var myFavNum= "10";
+console.log(typeof +myFavNum);
+console.log(typeofNumber(myFavNum));
